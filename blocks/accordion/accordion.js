275ef8cfe02b26103f8dd7b12fff7fd47b07381f
timwlee <timothyl@adobe.com>
2025-07-16 15:13:48 -0700

    Accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,19 +4,31 @@
  * https://www.hlx.live/developer/block-collection/accordion
  */
 
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     // decorate accordion item label
     const label = row.children[0];
     const summary = document.createElement('summary');
     summary.className = 'accordion-item-label';
+
+    // Move instrumentation from original label to new summary
+    moveInstrumentation(label, summary);
+
     summary.append(...label.childNodes);
+
     // decorate accordion item body
     const body = row.children[1];
     body.className = 'accordion-item-body';
+
     // decorate accordion item
     const details = document.createElement('details');
     details.className = 'accordion-item';
+
+    // Move instrumentation from row to details, if needed
+    moveInstrumentation(row, details);
+
     details.append(summary, body);
     row.replaceWith(details);
   });
